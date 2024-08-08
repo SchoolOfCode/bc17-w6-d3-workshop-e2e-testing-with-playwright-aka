723e61342da79a3712a8342a16ffcc2aa3c1e1bd
Adil <adilk23@outlook.com>
2024-08-08 12:21:22 +0100

    complete ticket 5 and ticket 6

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -3,4 +3,13 @@ import { test, expect  } from "@playwright/test";
 
 test ("practice test should pass", async ({page})  => {
     await page.goto ('http://localhost:3000/');
+    const input = page.getByRole('textbox');
+    await input.fill('team AKA is the best ! ! ')
 });
+
+//input element
+
+// <label for="new-todo">
+// New Todo:
+// <input type="text" id="new-todo" name="task" required />
+// </label>
